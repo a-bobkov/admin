1752801ceba0eba0cafc933a97d1d4fa19431105
avorobiev <avorobiev@maxposter.ru>
2014-01-24 16:16:38 +0400

    [dal/api] Удалил лишнюю функциою

--- web/js/dal/api.js
+++ web/js/dal/api.js
@@ -67,34 +67,6 @@ angular.module('app.dal.api', [])
             return $http.put(options.apiUrl+name, params, {withCredentials: true}).then(null,errorHandler);
         };
 
-        /**
-         * Generic method for sending a file
-         *
-         * @param name
-         * @param file
-         * @returns {Promise}
-         */
-        Api.postFile = function(name, file) {
-            var deferred = $q.defer();
-
-            var form = new FormData();
-            form.append('file', file);
-
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', options.apiUrl + name, true);
-            xhr.onload = function(e) {
-                if (e.target.status == 200) {
-                    deferred.resolve();
-                } else {
-                    deferred.reject();
-                }
-                if (!$rootScope.$$phase) $rootScope.$apply();
-            };
-            xhr.send(form);
-
-            return deferred.promise;
-        };
-
         return Api;
     };
 });
