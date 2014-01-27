629354914142e8be1b1d23bbbcb00dae8d268e5d
Andrey Bobkov <abobkov@maxposter.ru>
2014-01-27 16:41:11 +0100

    Переделаны тесты для метода api.put

--- test/unit/dal/api.spec.js
+++ test/unit/dal/api.spec.js
@@ -282,11 +282,164 @@ describe('У объекта app.dal.api', function() {
 
             expect(message).toBe('Ответ сервера не содержит данных');
         });
-
-
     });
 
-    describe('Метод remove()', function() {
+    describe('Метод put()', function() {
+        it('должен вызывать обработчик ошибок если код ответа не 2xx', function(){
+            var url = '/api2/users/999',
+                data = {
+                    id:  1,
+                    one: 'data',
+                    two: 'other data'
+                },
+                errorHandler;
+
+            errorHandler = jasmine.createSpy('errorHandler');
+            Api.setErrorHandler(errorHandler);
+
+            $httpBackend
+                .expectPUT(url, data)
+                .respond(404);
+
+            Api.put(url, data);
+
+            $httpBackend.flush();
+
+            expect(errorHandler).toHaveBeenCalled();
+        });
+
+        it('должен возвращать сообщение об ошибке при отсутствии параметра status в ответе', function(){
+            var url = '/api2/users/1',
+                data = {
+                    id:  1,
+                    one: 'data',
+                    two: 'other data'
+                },
+                message;
+
+            $httpBackend
+                .expectPUT(url, data)
+                .respond({});
+
+            Api.put(url, data).then(null, function(response) {
+                message = response;
+            });
+
+            $httpBackend.flush();
+
+            expect(message).toBe('Ответ сервера не соответствует формату JSend');
+        });
+
+        it('должен возвращать сообщение об ошибке при неверном значении status в ответе', function(){
+            var url = '/api2/users/1',
+                data = {
+                    id:  1,
+                    one: 'data',
+                    two: 'other data'
+                },
+                message;
+
+            $httpBackend
+                .expectPUT(url, data)
+                .respond({ status: 123 });
+
+            Api.put(url, data).then(null, function(response) {
+                message = response;
+            });
+
+            $httpBackend.flush();
+
+            expect(message).toBe('Сервер возвратил некорректный статус ответа: 123');
+        });
+
+        it('должен возвращать сообщение об ошибке если status="error"', function(){
+            var url = '/api2/users/1',
+                data = {
+                    id:  1,
+                    one: 'data',
+                    two: 'other data'
+                },
+                message;
+
+            $httpBackend
+                .expectPUT(url, data)
+                .respond({
+                    status: 'error',
+                    message: 'На сервере ошибка'
+                });
+
+            Api.put(url, data).then(null, function(response) {
+                message = response;
+            });
+
+            $httpBackend.flush();
+
+            expect(message).toBe('Сервер возвратил ошибку: На сервере ошибка');
+        });
+
+        it('должен возвращать сообщение об ошибке при отсутствии параметра data', function(){
+            var url = '/api2/users/1',
+                data = {
+                    id:  1,
+                    one: 'data',
+                    two: 'other data'
+                },
+                message;
+
+            $httpBackend
+                .expectPUT(url, data)
+                .respond({
+                    status: 'success'
+                });
+
+            Api.put(url, data).then(null, function(response) {
+                message = response;
+            });
+
+            $httpBackend.flush();
+
+            expect(message).toBe('Ответ сервера не содержит данных');
+        });
+
+        it('должен возвращать отправленные данные', function(){
+            var url = '/api2/users/1',
+                data,
+                expected,
+                actual;
+
+            data = {
+                id:  1,
+                one: 'data',
+                two: 'other data'
+            };
+
+            expected = angular.extend(data, {
+                ext: 'extra data'
+            });
+
+            $httpBackend
+                .expectPUT(url, data)
+                .respond({
+                    status: 'success',
+                    data: {
+                        user: expected
+                    }
+                });
+
+            Api.put(url, data).then(function(response) {
+                actual = response;
+            });
+
+            $httpBackend.flush();
+
+            var key;
+            for (key in data) {
+                expect(actual.user[key]).toBe(expected[key]);
+            }
+        });
+    });
+
+    xdescribe('Метод remove()', function() {
         it('должен вызывать URL и получать ответ', function(){
             var url = '/test/url',
                 expected = 'test string',
@@ -324,43 +477,4 @@ describe('У объекта app.dal.api', function() {
         });
     });
 
-    describe('Метод put()', function() {
-        it('должен отправлять данные и принимать ответ', function(){
-            var url = '/test/url',
-                data = {some: 'data'},
-                expected = 'test string',
-                actual;
-
-            $httpBackend
-                .expectPUT(url, data)
-                .respond(expected);
-
-            Api.put(url, data).then(function(response) {
-                actual = response;
-            });
-
-            $httpBackend.flush();
-
-            expect(actual.data).toBe(expected);
-        });
-
-        it('должен вызывать обработчик ошибок при сбое', function(){
-            var url = '/test/url',
-                errorHandler,
-                data = {some: 'data'};
-
-            errorHandler = jasmine.createSpy('errorHandler');
-            Api.setErrorHandler(errorHandler);
-
-            $httpBackend
-                .expectPUT(url, data)
-                .respond(404);
-
-            Api.put(url, data);
-
-            $httpBackend.flush();
-
-            expect(errorHandler).toHaveBeenCalled();
-        });
-    });
 });
