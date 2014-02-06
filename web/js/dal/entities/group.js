686537e0af2d319dfee33d8d310838fef1597b5c
Andrey Bobkov <abobkov@maxposter.ru>
2014-02-06 12:26:31 +0400

    Убрал начальное значение справочника groups

--- web/js/dal/entities/group.js
+++ web/js/dal/entities/group.js
@@ -28,10 +28,5 @@ angular.module('app.dal.entities.group', ['app.dal.entities.collection', 'app.da
 angular.module('app.dal.rest.group', ['app.dal.static.api'])
 
 .factory('groupApi', function(StaticApi) {
-    return new StaticApi([
-	    { 'id': 1, 'name': 'Катя'},
-	    { 'id': 2, 'name': 'Инна'},
-	    { 'id': 4, 'name': 'Потеряшки'},
-	    { 'id': 0, 'name': 'Без тэга'}
-	]);
+    return new StaticApi();
 });
