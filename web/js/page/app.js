8434e8a81b9431b474185533718ab37dfb93f0d2
Andrey Bobkov <abobkov@maxposter.ru>
2014-03-11 13:49:31 +0400

    Создан спиннер при смене url внутри приложения
    Conflicts:
    	web/admin.html

--- web/js/page/app.js
+++ web/js/page/app.js
@@ -4,4 +4,22 @@ angular.module('RootApp', ['UsersApp'])
 .config(function(ApiProvider){  // $dialogProvider,
 //    $dialogProvider.options({modalFade: true});
     ApiProvider.options.apiUrl = '/api2';
+})
+
+.controller('AppCtrl', function($scope) {
+
+	$scope.viewRouteSpinner = false;
+
+	$scope.$on('$routeChangeStart', function() {
+	  $scope.viewRouteSpinner = true;
+	});
+
+	$scope.$on('$routeChangeSuccess', function() {
+	  $scope.viewRouteSpinner = false;
+	});
+
+	$scope.$on('$routeChangeError', function() {
+	  $scope.viewRouteSpinner = false;
+	});
+
 });
